f9847bb6187f695a8f57a1391dad5b081e35a5dd
Roman Lykhnenko <roman.lykhnenko@project-a.com>
2015-08-01 18:57:06 +0200

    file with functions

---
+++ 2.R
@@ -0,0 +1,159 @@
+################################################################################
+# this file contains a set of function
+#
+#
+################################################################################
+
+# fun1 : 
+
+library(fOptions)
+
+# Valuation of the call price for each value of the underlying
+Call    = GBSOption(TypeFlag = "c", S = St, X = K, Time = tau, r = r, b = 0, sigma = si)  
+Callp   = attr(Call, "price")
+
+################################################################################
+# bs_call
+# 
+# Valuation of European call option in BSM model.
+# Analytical formula.
+#
+# Args:
+#
+#       S0:     initial stock
+# 
+#       K :     strike price
+# 
+#       T :     maturity date (in year fractions)
+# 
+#       r :     constant risk-free short rate
+# 
+#       sigma : volatility factor in diffusion term
+# 
+# returns:
+#
+#       value : present value of the European call option
+################################################################################
+
+bs_call_price <- function(S0, K, T, r, sigma) {
+
+  d1 = (log(S0 / K) + (r + 0.5 * sigma^2) * T) / (sigma * sqrt(T))
+  
+  d2 = (log(S0 / K) + (r - 0.5 * sigma^2) * T) / (sigma * sqrt(T))
+  
+  value = (S0 * pnorm(d1) - K * exp(-r * T) * pnorm(d2))
+  
+  return(value)
+}
+
+
+# Vega of European option in BSM model.
+# 
+# Parameters
+# ==========
+#   S0 : float
+# initial stock/index level
+# K : float
+# strike price
+# T : float
+# maturity date (in year fractions)
+# r : float
+# constant risk-free short rate
+# sigma : float
+# volatility factor in diffusion term
+# 
+# Returns
+# =======
+#   vega : float
+# partial derivative of BSM formula with respect
+# to sigma, i.e. Vega
+
+bs_vega <- function(S0, K, T, r, sigma) {
+  
+  d1 = (log(S0 / K) + (r + 0.5 * sigma^2) * T) / (sigma * sqrt(T))
+  
+  vega = S0 * pnorm(d1) * sqrt(T)
+  
+  return(vega)
+
+}
+
+
+
+# Implied volatility of European call option in BSM model
+# 
+# Parameters
+# ==========
+#   S0 : float
+# initial stock/index level
+# K : float
+# strike price
+# T : float
+# maturity date (in year fractions)
+# r : float
+# constant risk-free short rate
+# sigma_est : float
+# estimate of impl. volatility
+# it : integer
+# number of iterations
+# 
+# Returns
+# =======
+#   simga_est : float
+# numerically estimated implied volatility
+
+
+bs_call_imp_vol <- function(S0, K, T, r, C0, sigmaEst, it=100) {
+
+  for( i in 1:it){
+    sigma_est <- sigmaEst - ((bs_call_price(S0, K, T, r, sigmaEst) - C0)
+                  / bs_vega(S0, K, T, r, sigmaEst))
+  }
+    
+  return(sigma_est)
+}
+
+
+# performance ##################################################################
+
+S0 <- 100
+K <- 105
+T <- 1
+r <- 0.05
+sigmaEst <- 0.2
+sigma <- sigmaEst
+C0 <- 10
+
+bs_call_price(S0, K, T, r, sigma)
+
+bs_vega(S0, K, T, r, sigma)
+
+bs_call_imp_vol(S0, K, T, r, C0, sigmaEst, it=100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
