4e7d90cb6150d338dab4e81b08466e76a478e470
Roman Lykhnenko <roman.lykhnenko@project-a.com>
2015-08-20 19:00:28 +0200

    file with functions used in code/app

---
+++ functions.R
@@ -0,0 +1,56 @@
+
+
+
+# function: bsCallPrice
+# 
+# Valuation of European call option in BSM model.
+# Analytical formula.
+#
+# Args:
+#
+#       S0:     initial stock price
+# 
+#       K :     strike price
+# 
+#       T :     maturity date (in year fractions)
+# 
+#       r :     constant risk-free short rate
+# 
+#       sigma : volatility 
+# 
+# returns:
+#
+#       value : present value of the European call option
+
+bsCallPrice <- function(S0, K, T, r, sigma) {
+  
+  d1 = (log(S0 / K) + (r + 0.5 * sigma^2) * T) / (sigma * sqrt(T))
+  
+  d2 = (log(S0 / K) + (r - 0.5 * sigma^2) * T) / (sigma * sqrt(T))
+  
+  value = (S0 * pnorm(d1) - K * exp(-r * T) * pnorm(d2))
+  
+  return(value)
+}
+
+
+bsVega <- function(S0, K, T, r, sigma) {
+  
+  d1 = (log(S0 / K) + (r + 0.5 * sigma^2) * T) / (sigma * sqrt(T))
+  
+  vega = S0 * dnorm(d1) * sqrt(T)
+  
+  return(vega)
+  
+}
+
+bsCallImpVol <- function(S0, K, T, r, C0, sigmaEst, it=100) {
+  
+  for( i in 1:it){
+    sigmaEst <- sigmaEst - ((bs_call_price(S0, K, T, r, sigmaEst) - C0)
+                            / bs_vega(S0, K, T, r, sigmaEst))
+  }
+  
+  return(sigmaEst)
+}
+
